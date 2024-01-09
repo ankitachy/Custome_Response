384074f496d85488410b084d65ac78181bde07f5
awesomehitesh23 <awesomehitesh23@gmail.com>
2024-01-09 21:02:25 +0530

    Initial

--- src/main/java/com/stackroute/customresponse/controller/ProductController.java
+++ src/main/java/com/stackroute/customresponse/controller/ProductController.java
@@ -1,4 +1,5 @@
 package com.stackroute.customresponse.controller;
+import java.lang.module.ResolutionException;
 import java.util.List;
 
 import com.stackroute.customresponse.model.Product;
@@ -11,7 +12,6 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,25 +26,45 @@ public class ProductController {
     //use try catch and return ResponseHandler with "Successfully retrieved data!" message
     @GetMapping("/products")
     public ResponseEntity<Object> getAllProduct() {
-      return null;
+    	 try {
+             List<Product> product=productService.getAllProduct();
+             return ResponseHandler.generateResponse("Successfully retrieved data!", HttpStatus.OK, product);
+         } catch (ResolutionException e) {
+             return ResponseHandler.generateResponse(e.getMessage(), HttpStatus.MULTI_STATUS, null);
+         }
     }
 
     //use try catch and return ResponseHandler with "Successfully retrieved data!" message
     @GetMapping("/products/{id}")
     public ResponseEntity < Object > getProductById(@PathVariable long id) {
-       return null;
+    	 try {
+            Product product=productService.getProductById(id);
+             return ResponseHandler.generateResponse("Successfully retrieved data!", HttpStatus.OK, product);
+         } catch (ResolutionException e) {
+             return ResponseHandler.generateResponse(e.getMessage(), HttpStatus.MULTI_STATUS, null);
+         }
     }
 
     //use try catch and return ResponseHandler with "Successfully added data!" message
     @PostMapping("/products")
     public ResponseEntity < Object > createProduct(@RequestBody Product product) {
-       return null;
+    	try {
+            Product product1=productService.createProduct(product);
+             return ResponseHandler.generateResponse("Successfully added data!", HttpStatus.CREATED, product1);
+         } catch (ResolutionException e) {
+             return ResponseHandler.generateResponse(e.getMessage(), HttpStatus.MULTI_STATUS, null);
+         }
     }
 
     //use try catch and return ResponseHandler with "Deleted!" message
     @DeleteMapping("/products/{id}")
     public ResponseEntity<Object> deleteProduct(@PathVariable long id) {
-        return null;
+    	try {
+            Product product1=productService.deleteProduct(id);
+             return ResponseHandler.generateResponse("Deleted!", HttpStatus.OK, product1);
+         } catch (ResolutionException e) {
+             return ResponseHandler.generateResponse(e.getMessage(), HttpStatus.MULTI_STATUS, null);
+         }
     }
 
     }
